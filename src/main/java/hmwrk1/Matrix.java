96074f7130016297f9796251311d26f1c42d1eff
danny73210 <danny73210@gmail.com>
2018-12-06 19:12:07 +0200

    commit

--- src/main/java/hmwrk1/Matrix.java
+++ src/main/java/hmwrk1/Matrix.java
@@ -1,18 +1,23 @@
 package hmwrk1;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Matrix {
-    public static void main(String[] args) {
-        int[][] mas = {{1, 2, 3, 4, 5},
-                {-2, -3, -4, -5, -6},
-                {3, 4, 5, 6, 7},
-                {-4, -5, -6, -7, -8},
-                {5, 6, 7, 8, 9}};
 
-        Matrix.printMatrix(mas);
-        System.out.println("reflected matrix: \n");
-        Matrix.reflectMatrix(mas);
-        Matrix.printMatrix(mas);
-
+    public static void wtireToFile(int[][] matrix,String path) {
+        try (FileWriter writer = new FileWriter(path)) {
+            for (int i = 0; i < matrix.length; i++) {
+                String text = Arrays.toString(matrix[i]);
+                writer.append(text.substring(1, text.length() - 1));
+                writer.append('\n');
+            }
+            writer.close();
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     public static void reflectMatrix(int[][] mas) {
